de789a86913867ddca45db5db58825393a6eb697
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-21 15:57:39 -0700

    added schema

---
+++ techNotes/models/Note.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
+
+const noteSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    //this is an array, data within will be a string
+    text: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+},
+{
+//timestamps are options in mongoDB
+timestamps: true
+}
+
+
+)
+//use sequence to make ticket numbers
+noteSchema.plugin(AutoIncrement, {
+    inc_field: 'ticket',
+    id: 'ticketNums',
+    start_seq: 500
+})
+
+module.exports = mongoose.model('Note', noteSchema)
---
+++ techNotes/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    //data model goes here
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    //this is an array, data within will be a string
+    roles: [{
+        type: String,
+        default:"Employee"
+    }],
+    active: {
+        type: Boolean,
+        default: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
