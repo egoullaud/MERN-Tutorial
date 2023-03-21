8a5c6d490612f650c4a592a615aaf9782c786f09
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-21 13:46:56 -0700

    added logs!

---
+++ techNotes/middleware/logger.js
@@ -0,0 +1,34 @@
+//destructure format
+
+const {format} = requre('date-fns')
+const {v4: uuid} = require('uuid')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path=require('path')
+
+const logEvents = async (message, logFileName) => {
+    const dateTime = `${format(new Date(), 'yyyMMdd\tHH:mm:ss')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+
+//look for logs folder
+    try{
+        //if no folder found, let's create one
+        if(!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs',
+         logFileName), logItem)
+    } catch(err) {
+        console.log(err)
+    }
+}
+//write middleware
+const logger= (req, res, next) =>{
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    //recommends adding conditions to limit which requests are logged (only from other urls or something)
+    //otherwise the log will fill up fast.
+    console.log(`${req.method} ${req.path}`)
+    next()
+}
+
+module.exports = {logEvents, logger}
